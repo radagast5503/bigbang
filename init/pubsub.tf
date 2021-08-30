ed301a96328dee246bbd917f91e42ee5c6952fe1
ferjjp <fer.jjp@gmail.com>
2021-08-29 22:40:34 -0300

    Add pubsub notif.

--- init/pubsub.tf
+++
@@ -1,7 +0,0 @@
-resource "google_pubsub_topic" "image_dropbox" {
-    name = "image-dropbox"
-    labels = {
-        team = "radagast5503"
-    }
-    project = google_project.main.project_id
-}
---
+++ init/storage.tf
@@ -0,0 +1,68 @@
+resource "google_pubsub_topic" "image_dropbox" {
+    name = "image-dropbox"
+    labels = {
+        team = "radagast5503"
+    }
+    project = google_project.main.project_id
+}
+
+resource "google_pubsub_topic_iam_binding" "binding" {
+  topic   = google_pubsub_topic.image_dropbox.id
+  role    = "roles/pubsub.publisher"
+  members = [local.service_account_name]
+}
+
+//================================================
+
+resource "google_storage_bucket" "terraform" {
+    name = local.terraform_bucket_name
+    project = google_project.main.project_id
+    force_destroy = true
+    storage_class = "STANDARD"
+    labels = {
+        team = "muebles-ra"
+    }
+}
+
+resource "google_storage_bucket" "furniture" {
+  name          = local.furniture_bucket_name
+  project       = google_project.main.project_id
+  force_destroy = true
+  storage_class = "STANDARD"
+  labels = {
+    team = "radagast5503"
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 4 # vida de muebles
+    }
+
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+resource "google_storage_bucket_iam_member" "service_account_storage_project_admin_on_furniture" {
+  bucket = google_storage_bucket.furniture.name
+  role   = "roles/storage.admin"
+  member = local.service_account_name
+}
+
+resource "google_storage_bucket_iam_member" "service_account_storage_render_admin_on_furniture" {
+  bucket = google_storage_bucket.furniture.name
+  role   = "roles/storage.admin"
+  member = var.render_service_account
+}
+
+resource "google_storage_notification" "notification" {
+    bucket = google_storage_bucket.furniture
+    payload_format = "JSON_API_V1"
+    topic = google_pubsub_topic.image_dropbox
+    event_types = ["OBJECT_FINALIZE"]
+    custom_attributes = {
+      "project" = google_project.main.project_id
+    }
+}
+//=========================================================
